0f4569efe5f480490be484790116efbba264cccb
Blen <blenassefa00@gmail.com>
2025-07-15 12:17:27 +0200

    Init: initialized the backend spring boot app

---
+++ backend/src/main/java/com/book/inventory/management/bookim/BookimApplication.java
@@ -0,0 +1,13 @@
+package com.book.inventory.management.bookim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookimApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/book/inventory/management/bookim/BookimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.inventory.management.bookim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
